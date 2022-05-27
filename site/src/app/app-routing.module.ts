cb7391e0e6ffcea0c05ddf51f9c086c6474f2bd7
alex10planells@gmail.com <alex10planells@gmail.com>
2022-05-27 20:54:42 +0200

    add html: Footer, Card component, Admin list, Player list

--- site/src/app/app-routing.module.ts
+++ site/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AuthComponent} from "./entities/auth/auth.component";
-import {SongListComponent} from "./entities/song/song-list/song-list.component";
+import {SongPlayerListComponent} from "./entities/song/player/song-player-list/song-player-list.component";
+import {SongAdminListComponent} from "./entities/song/admin/song-admin-list/song-admin-list.component";
 
 const routes: Routes = [
   { path: '', component: AuthComponent, pathMatch: 'full' },
-  { path: 'profile/:profileName', component: SongListComponent}
+  { path: 'admin', component: SongAdminListComponent },
+  { path: 'profile/:profileName', component: SongPlayerListComponent }
 ];
 
 @NgModule({
